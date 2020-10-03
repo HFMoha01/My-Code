7a2d52ca6d734f0e78d0e4b83bb5ad63deddb03a
HFMoha01 <64575483+HFMoha01@users.noreply.github.com>
2020-10-02 23:21:27 -0400

    Update LibraryJournal.cs

--- Software Development II/Prog1B/Prog1A/Prog1/LibraryJournal.cs
+++ Software Development II/Prog1B/Prog1A/Prog1/LibraryJournal.cs
@@ -1,8 +1,7 @@
 ﻿// Program 1A
 // CIS 200-01
 // Due: 2/13/2020
-// By: Andrew L. Wright (Students use Grading ID)
-
+// By: Hasan Mohammad
 // File: LibraryJournal.cs
 // This file creates a concrete LibraryJournal class that adds
 // discipline and editor.
