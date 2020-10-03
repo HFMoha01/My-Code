9076572a6cb7d5cc69afea278a0b9786a6d79887
HFMoha01 <64575483+HFMoha01@users.noreply.github.com>
2020-10-02 23:17:23 -0400

    Update LibraryMovie.cs

--- Software Development II/Prog2/Prog2-Start/Prog2/Prog2/LibraryMovie.cs
+++ Software Development II/Prog2/Prog2-Start/Prog2/Prog2/LibraryMovie.cs
@@ -1,7 +1,7 @@
 ﻿// Program 1A
 // CIS 200-01
 // Due: 2/13/2020
-// By: Andrew L. Wright (Students use Grading ID)
+// By: Hasan Mohammad
 
 // File: LibraryMovie.cs
 // This file creates a concrete LibraryMovie class that adds
