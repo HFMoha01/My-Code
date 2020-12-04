c8b524da9f1b46aad6bb2995e6399bd99e747406
Hasan F Mohammad <hfmoha01@louisville.edu>
2020-12-04 00:51:33 -0500

    ASP
    ADD New Repository

---
+++ ASP.NET/Office of Research&InWebsite/Areas/Students/Controllers/GraduateController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS_420_Project.Areas.Students.Controllers
+{
+    public class GraduateController : Controller
+    {
+        [Area("Students")]
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+    }
+}
